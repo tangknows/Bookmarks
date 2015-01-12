74cb33e42a7fc8a70ffa730c622fefae44556cae
thanh ngo <ngvt@live.com>
2015-01-11 18:24:39 -0600

    debug4

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # app/mailers/application_mailer.rb
-class ApplicationMailer < ActionMailer::Base
-  default 'ngvt@live.com'
-  layout 'mailer'
-end
+#class ApplicationMailer < ActionMailer::Base
+#  default 'ngvt@live.com'
+#  layout 'mailer'
+#end
  
