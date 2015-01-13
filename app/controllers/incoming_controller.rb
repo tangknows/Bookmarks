3e231b85fe01b86d4f5ebfae4a4514d41435884c
thanh ngo <ngvt@live.com>
2015-01-12 18:03:14 -0600

    parse e-mail

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,6 +8,13 @@ class IncomingController < ApplicationController
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.2
     puts "INCOMING PARAMS HERE: #{params}"
+    
+    @subject = Incoming.new(params[:subject])
+    @subject.save
+    
+    puts "This is the #{@subject}"
+    puts "This is the #{subject2}"
+    
 
     # You put the message-splitting and business
     # magic here. 
