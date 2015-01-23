cb1cd08a43dc7f771d79ab61c8a6503e27ad17fa
thanh ngo <ngvt@live.com>
2015-01-22 18:33:16 -0600

    topic controller show all 5

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,8 +4,10 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    
     puts "INCOMING PARAMS HERE: #{params}"
-    puts params[:subject]
+    sender = params[:sender]
+    puts sender
+    Topic.create(:title => params[:sender], :topic => params[:sender])
+    head 200
   end
 end
